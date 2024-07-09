954913d9776877d5ad6fe3ee928afcd87913b890
Hoilazer <castortroymendoza@gmail.com>
2024-07-09 17:27:15 +0800

    Delete Bruh directory

--- Bruh/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").addEventListener("submit", function(event) {
-        let isValid = true;
-        this.querySelectorAll("input, textarea").forEach(function(input) {
-            if (!input.value) {
-                isValid = false;
-                input.classList.add("is-invalid
